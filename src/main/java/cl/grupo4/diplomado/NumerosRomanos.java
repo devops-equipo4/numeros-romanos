796d67e3209837600681d7e4e735e479fae1797d
luismella <luismellareyes@gmail.com>
2022-03-05 10:37:00 -0300

    Codigo de numeros romandos

---
+++ src/main/java/cl/grupo4/diplomado/NumerosRomanos.java
@@ -0,0 +1,19 @@
+package cl.grupo4.diplomado;
+
+import cl.grupo4.diplomado.conversiones.ConvertirANumerosRomanos;
+
+import java.util.Scanner;
+
+public class NumerosRomanos {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        do {
+            System.out.print("Introduce un número entre 1 y 3999: ");
+            N = sc.nextInt();
+        } while (N < 1 || N > 3999);
+        ConvertirANumerosRomanos convertirANumerosRomanos = new ConvertirANumerosRomanos();
+        System.out.println(N + " en numeros romanos -> " + convertirANumerosRomanos.convertirANumerosRomanos(N));
+    }
+}
---
+++ src/test/java/cl/grupo4/diplomado/conversion/ConvertirANumeroRomanosTest.java
@@ -0,0 +1,17 @@
+package cl.grupo4.diplomado.conversion;
+
+import org.junit.Test;
+
+public class ConvertirANumeroRomanosTest {
+
+    @Test
+    public void unidadTest() {
+    }
+
+    @Test
+    public void decenasTest() {
+    }
+    @Test
+    public void centenasTest() {
+    }
+}
